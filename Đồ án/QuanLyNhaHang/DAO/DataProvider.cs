8af1923c1a0dd270ab8bef28b7c80341736f5fe3
DRyu1412 <72917241+DRyu1412@users.noreply.github.com>
2020-11-10 19:08:24 +0700

    update DAO DataProvider

---
+++ Đồ án/QuanLyNhaHang/DAO/DataProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaHang.DAO
+{
+    public class DataProvider
+    {
+        private string connectionStr = @"Data Source=.\SQLEXPRESS;Initial Catalog=QuanLyNhaHang;Integrated Security=True";
+
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;          
+        }
+    }
+}
